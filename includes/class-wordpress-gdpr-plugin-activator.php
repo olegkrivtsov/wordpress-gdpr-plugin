f2f61d31e43e543c863a90187cb28fff2b39de85
Oleg Krivtsov <olegkrivtsov@gmail.com>
2018-06-22 16:39:30 +0600

    Added files

---
+++ includes/class-wordpress-gdpr-plugin-activator.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    WordPress_Gdpr_Plugin
+ * @subpackage WordPress_Gdpr_Plugin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    WordPress_Gdpr_Plugin
+ * @subpackage WordPress_Gdpr_Plugin/includes
+ * @author     Oleg Krivtsov <olegkrivtsov@gmail.com>
+ */
+class WordPress_Gdpr_Plugin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+        
+    
+	}
+
+}
---
+++ includes/class-wordpress-gdpr-plugin-deactivator.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    WordPress_Gdpr_Plugin
+ * @subpackage WordPress_Gdpr_Plugin/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    WordPress_Gdpr_Plugin
+ * @subpackage WordPress_Gdpr_Plugin/includes
+ * @author     Oleg Krivtsov <olegkrivtsov@gmail.com>
+ */
+class WordPress_Gdpr_Plugin_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+        
+	}
+
+}
